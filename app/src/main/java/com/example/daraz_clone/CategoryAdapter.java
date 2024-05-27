47ba828857fc76d02c7d44e6f65119faaf7ae932
waqasali <programmingwithranawaqas@gmail.com>
2024-05-27 15:35:28 +0500

    Updated code

--- app/src/main/java/com/example/daraz_clone/CategoryAdapter.java
+++ app/src/main/java/com/example/daraz_clone/CategoryAdapter.java
@@ -1,25 +1,28 @@
 package com.example.daraz_clone;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
 
     ArrayList<Category> categories;
+    Context context;
     public CategoryAdapter(Context context, ArrayList<Category> categories)
     {
         this.categories = categories;
+        this.context = context;
     }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -31,9 +34,11 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Category category = categories.get(position);
+        Category category = categories.get(holder.getAdapterPosition());
+
         holder.tvCategoryName.setText(category.getName());
         holder.tvCountSubCategory.setText(category.getSubCategories()+"");
+
         if (category.getImgUrl().equals("clothes"))
         {
             holder.ivCategoryPic.setImageResource(R.drawable.clothes);
@@ -52,6 +57,63 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         }
 
 
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                AlertDialog.Builder editDelete = new AlertDialog.Builder(context);
+                editDelete.setTitle("Update or Delete Category");
+                View view = LayoutInflater.from(context)
+                                .inflate(R.layout.form_to_add_new_category,  null, false);
+                EditText etName = view.findViewById(R.id.etName);
+                EditText etSubCategory = view.findViewById(R.id.etSubcategory);
+                EditText etImageUrl = view.findViewById(R.id.etImageUrl);
+
+                editDelete.setView(view);
+                etName.setText(category.getName());
+                etSubCategory.setText(category.getSubCategories()+"");
+                etImageUrl.setText(category.getImgUrl());
+
+                editDelete.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = etName.getText().toString().trim();
+                        String subCategory = etSubCategory.getText().toString();
+                        String imgUrl = etImageUrl.getText().toString().trim();
+
+                        if(name.isEmpty() || subCategory.isEmpty() || imgUrl.isEmpty())
+                        {
+                            Toast.makeText(context, "Something is missing in the form", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            String oldName = category.getName();
+                            category.setName(name);
+                            category.setSubCategories(Integer.parseInt(subCategory));
+                            category.setImgUrl(imgUrl);
+                            Toast.makeText(context, oldName+" has been updated", Toast.LENGTH_SHORT).show();
+                            notifyDataSetChanged();
+                        }
+
+                    }
+                });
+
+                editDelete.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MyApplication.categories.remove(holder.getAdapterPosition());
+                        Toast.makeText(context, category.getName()+" Deleted...", Toast.LENGTH_SHORT).show();
+                        notifyDataSetChanged();
+                    }
+                });
+
+                editDelete.show();
+
+
+                return false;
+            }
+        });
 
     }
 
